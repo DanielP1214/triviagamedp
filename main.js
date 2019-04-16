8144f5dd32bf21b97183d5e4009162f2eb841021
Puerta <0320409@students.dadeschools.net>
2019-04-16 14:08:15 -0400

    finihed html started javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question set
+var questions = [
+    
+    {
+        question: "When was the laptop invented?",
+        answers: ["1981", "2002", "2000", "1976"]
+        correctAnswer: "1981",
+    }
+
+]
